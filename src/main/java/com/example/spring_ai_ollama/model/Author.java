81443ccc0f1b57faf9e3f986fa78e6c098b7d387
I Nyoman Surya Pradipta <suryapradipta8@gmail.com>
2025-02-09 23:08:28 +0800

    map and bean output converter

---
+++ src/main/java/com/example/spring_ai_ollama/model/Author.java
@@ -0,0 +1,19 @@
+package com.example.spring_ai_ollama.model;
+
+public class Author {
+    private String name;
+    private String[] books;
+
+    public Author(String name, String[] books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
+}
